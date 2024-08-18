449ffab1fa7c9dc00a8fef464a0a972aaabeaf70
ElenLen <relax-pl@yandex.ru>
2024-08-18 12:53:31 +0500

    fix: finish

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,20 +1,14 @@
 // Создайте функцию, которая выведет сообщение в консоли «Я учу JavaScript!».
-function myLern() {
-  console.log('Я учу JavaScript!');
-}
+const myLern = () => console.log("Я учу JavaScript!");
 myLern();
 
-// получить элемент
-const image = document.getElementById('cat1');
+// получить элемент картинки
+const image = document.getElementById("cat");
 console.log(image);
 
 // функция
-function previous() {
-  image.src = "./assets/style/img/cat1.jpg"
-}
-
-function next() {
-  image.src = "./assets/style/img/cat2.jpg"
-}
-console.log(next()) ;
+const previous = () => (image.src = "./assets/style/img/cat1.jpg");
 console.log(previous());
+
+const next = () => (image.src = "./assets/style/img/cat2.jpg");
+console.log(next());
